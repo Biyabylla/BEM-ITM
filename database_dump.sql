b2b39e583b77c6c8b26c4b36a3ad9a10d4bacab5
Nabila Putri Ardina <ardinanabila35@gmail.com>
2026-08-29 19:01:40 +0700

    config: dual environment (XAMPP + InfinityFree), database.sql auto-create DB, hapus database_dump.sql

---
+++ database_dump.sql
@@ -0,0 +1,8 @@
+﻿mysqldump.exe : mysqldump.exe: Got error: 2002: "Can't connect to MySQL server on 'localhost' (10061)" when trying to 
+connect
+At line:1 char:1
++ & "C:\xampp\mysql\bin\mysqldump.exe" -u root -pnabiyA050505_ --no-cre ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (mysqldump.exe: ...ying to connect:String) [], RemoteException
+    + FullyQualifiedErrorId : NativeCommandError
+ 
